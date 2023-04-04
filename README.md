dd9e017b3bdf888830ddd208939815dc020ea035
TheNefelin <metalflap@gmail.com>
2023-04-03 22:29:13 -0400

    Update

---
+++ README.md
@@ -0,0 +1,7 @@
+# M7-ABPRO-4
+
+> dependencias
+```
+npm install express
+npm install pg
+```
---
+++ postgreSQL.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE Banco;
+
+CREATE TABLE transacciones
+(descripcion varchar(50), fecha varchar(10), monto DECIMAL, cuenta INT);
+
+CREATE TABLE cuentas (id INT, saldo DECIMAL CHECK (saldo >= 0));
+
+INSERT INTO cuentas values (1, 20000);
